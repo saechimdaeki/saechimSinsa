63ae939a7bac53876889c1b5590fe65e8493de52
Junseong kim <anima94@kakao.com>
2024-11-11 16:43:49 +0900

    Initial commit

---
+++ src/test/java/me/saechimdaeki/sinsa/SinsaApplicationTests.java
@@ -0,0 +1,13 @@
+package me.saechimdaeki.sinsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SinsaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
